fa2b7877b6b705e98cb88fc15e5c5ee757f048e6
Özlem Balıkçı <balikciozlem.381@gmail.com>
2025-12-23 19:26:01 +0300

    Push project: Retinal Disease Classification

---
+++ metrics/f1score.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow.keras import backend as K
+
+class F1Score(tf.keras.metrics.Metric):
+    def __init__(self, name='f1_score', threshold=0.5, **kwargs):
+        super(F1Score, self).__init__(name=name, **kwargs)
+        self.threshold = threshold
+        self.tp = self.add_weight(name='tp', initializer='zeros')
+        self.fp = self.add_weight(name='fp', initializer='zeros')
+        self.fn = self.add_weight(name='fn', initializer='zeros')
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.cast(y_pred > self.threshold, tf.float32)
+        y_true = tf.cast(y_true, tf.float32)
+
+        tp = tf.reduce_sum(y_true * y_pred)
+        fp = tf.reduce_sum((1 - y_true) * y_pred)
+        fn = tf.reduce_sum(y_true * (1 - y_pred))
+
+        self.tp.assign_add(tp)
+        self.fp.assign_add(fp)
+        self.fn.assign_add(fn)
+
+    def result(self):
+        precision = self.tp / (self.tp + self.fp + K.epsilon())
+        recall = self.tp / (self.tp + self.fn + K.epsilon())
+        f1 = 2 * precision * recall / (precision + recall + K.epsilon())
+        return f1
+
+    def reset_state(self):
+        self.tp.assign(0)
+        self.fp.assign(0)
+        self.fn.assign(0)
